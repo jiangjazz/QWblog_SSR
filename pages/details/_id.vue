cb5e5643c3c3f2f57af9ec3881677ffbbbb06df0
王文宇 <wangwenyu9394@gmail.com>
2017-09-19 08:20:32 +0800

    :lipstick: Update 更换代码高亮主题

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -89,89 +89,81 @@
         font-weight: 400;
         color:#666;
       }
-      /* Base16 Atelier Savanna Dark - Theme */
-      /* by Bram de Haan (http://atelierbram.github.io/syntax-highlighting/atelier-schemes/savanna) */
-      /* Original Base16 color scheme by Chris Kempson (https://github.com/chriskempson/base16) */
 
-      /* Atelier-Savanna Comment */
-      .hljs-comment,
-      .hljs-quote {
-        color: #78877d;
+      pre{
+        padding:0;
       }
 
-      /* Atelier-Savanna Red */
-      .hljs-variable,
-      .hljs-template-variable,
-      .hljs-attribute,
-      .hljs-tag,
-      .hljs-name,
-      .hljs-regexp,
-      .hljs-link,
-      .hljs-name,
-      .hljs-selector-id,
-      .hljs-selector-class {
-        color: #b16139;
-      }
-
-      /* Atelier-Savanna Orange */
-      .hljs-number,
-      .hljs-meta,
-      .hljs-built_in,
-      .hljs-builtin-name,
-      .hljs-literal,
-      .hljs-type,
-      .hljs-params {
-        color: #9f713c;
-      }
-
-      /* Atelier-Savanna Green */
-      .hljs-string,
-      .hljs-symbol,
-      .hljs-bullet {
-        color: #489963;
-      }
-
-      /* Atelier-Savanna Blue */
-      .hljs-title,
-      .hljs-section {
-        color: #478c90;
-      }
-
-      /* Atelier-Savanna Purple */
-      .hljs-keyword,
-      .hljs-selector-tag {
-        color: #55859b;
-      }
-
-      .hljs-deletion,
-      .hljs-addition {
-        color: #171c19;
-        display: inline-block;
-        width: 100%;
-      }
-
-      .hljs-deletion {
-        background-color: #b16139;
-      }
-
-      .hljs-addition {
-        background-color: #489963;
-      }
 
       .hljs {
         display: block;
         overflow-x: auto;
-        background: #171c19;
-        color: #87928a;
         padding: 0.5em;
+        background: #2b2b2b;
+      }
+
+      .hljs {
+        color: #bababa;
+      }
+
+      .hljs-strong,
+      .hljs-emphasis {
+        color: #a8a8a2;
+      }
+
+      .hljs-bullet,
+      .hljs-quote,
+      .hljs-link,
+      .hljs-number,
+      .hljs-regexp,
+      .hljs-literal {
+        color: #6896ba;
+      }
+
+      .hljs-code,
+      .hljs-selector-class {
+        color: #a6e22e;
       }
 
       .hljs-emphasis {
         font-style: italic;
       }
 
-      .hljs-strong {
-        font-weight: bold;
+      .hljs-keyword,
+      .hljs-selector-tag,
+      .hljs-section,
+      .hljs-attribute,
+      .hljs-name,
+      .hljs-variable {
+        color: #cb7832;
+      }
+
+      .hljs-params {
+        color: #b9b9b9;
+      }
+
+      .hljs-string {
+        color: #6a8759;
+      }
+
+      .hljs-subst,
+      .hljs-type,
+      .hljs-built_in,
+      .hljs-builtin-name,
+      .hljs-symbol,
+      .hljs-selector-id,
+      .hljs-selector-attr,
+      .hljs-selector-pseudo,
+      .hljs-template-tag,
+      .hljs-template-variable,
+      .hljs-addition {
+        color: #e0c46c;
+      }
+
+      .hljs-comment,
+      .hljs-deletion,
+      .hljs-meta {
+        color: #7f7f7f;
       }
 
     }

9d93c3f3df33839c0851a9b0b60b14b53c52b48b
王文宇 <wangwenyu9394@gmail.com>
2017-09-20 21:51:08 +0800

    :lipstick: Update footer样式

--- components/Copyright.vue
+++ components/Copyright.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="m-footer f-cb">
     <div class="g-box">
-      <p>Copyright 2017 – Created by 优雅的<a href="https://laravel.com/" target="_blank">Laravel</a> && 美妙的 <a href="https://cn.vuejs.org/v2/guide/" target="_blank">Vue.js</a> && 强悍的 <a
-        href="https://zh.nuxtjs.org/" target="_blank">Nuxt.js</a>    &nbsp&nbsp备案号：皖ICP备14022073号-4</p>
+      <p>Copyright 2017 – 备案号：皖ICP备14022073号-4</p>
+      <p>Created by 优雅的<a href="https://laravel.com/" target="_blank">Laravel</a> && 美妙的 <a href="https://cn.vuejs.org/v2/guide/" target="_blank">Vue.js</a> && 强悍的 <a
+        href="https://zh.nuxtjs.org/" target="_blank">Nuxt.js</a></p>
     </div>
   </div>
 </template>
@@ -26,12 +27,13 @@
     font-size: 12px;
     font-weight: 500;
     margin-top: 15px;
-    height:48px;
+    padding:10px 0;
     clear: both;
     width: 100%;
     float: left;
+    text-align: center;
     p{
-      line-height: 48px;
+      line-height: 26px;
     }
     @media (max-width: 1023px){
       padding: 10px 50px;
